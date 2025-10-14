5e418185f159e3bbbd370488cfbdf7365611f3ea
Peter Mwaura <kimmwaus@gmail.com>
2025-10-14 15:08:11 +0300

    update to tips additions

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ use App\Models\Transaction;
 use App\Http\Controllers\Api\UserPackagesController;
 use Illuminate\Validation\Rule;
 
-
+//display
 // Public
 Route::get('/leagues', [LeagueController::class, 'index']);
 Route::get('/teams', [TeamController::class, 'index']);
