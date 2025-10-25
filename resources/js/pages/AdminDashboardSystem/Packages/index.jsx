5f995d44d52397e75e4ee995b3e5f88989ba6fa3
Peter Mwaura <kimmwaus@gmail.com>
2025-10-25 17:15:13 +0300

    enable redirect

--- resources/js/pages/AdminDashboardSystem/Packages/index.jsx
+++ resources/js/pages/AdminDashboardSystem/Packages/index.jsx
@@ -140,7 +140,7 @@ export default function Index() {
                 onSuccess: () => {
                     toast.success('Subscription updated successfully');
                     setTimeout(() => {
-                        router.visit(route('admin.packages.index'))
+                        window.location.reload()
                     })
                 },
                 onError: (errors) => {
