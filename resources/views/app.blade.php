f1a36ead528281be2ae6cff2eb1acf2ef95372fc
Peter Mwaura <kimmwaus@gmail.com>
2026-03-26 12:11:22 +0300

    update to pixel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,23 +30,6 @@
         }
     </style>
 
-    <!-- Meta Pixel Code latest -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1594458748878592');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1594458748878592&ev=PageView&noscript=1"
-        /></noscript>
-    <!-- End Meta Pixel Code -->
 
     <!-- Meta Pixel Code -->
     <script>
@@ -71,6 +54,25 @@
         fbq('init', '1192867339189991');
         fbq('track', 'PageView');
     </script>
+
+    <!-- Meta Pixel Code latest -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1594458748878592');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=1594458748878592&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <noscript>
         <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1192867339189991&ev=PageView&noscript=1" />
     </noscript>
